3e4dc37125d501cb31d437c4ffda2d058c0fe7c9
Mostafa <musstafa.muhammed@gmail.com>
2026-04-27 16:52:06 +0300

    check file modified

--- check.sh
+++ check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #This Script checks the system health
 DISK=$(df -h / | grep / | awk '{print $5}' | cut -d% -f1)
-
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }'
 echo "Checking Disk Usage..."
 
 if [ $DISK -gt 80 ]; then
